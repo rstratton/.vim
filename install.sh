1ff038c20cfc14df1d863c3948e6e868e0f58503
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-04 14:53:36 -0800

    Add download of alternative molokai colorscheme to install script

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ then
 fi
 ln -s `pwd`/.vimrc ~/.vimrc
 
+curl "https://raw.github.com/tomasr/molokai/master/colors/molokai.vim" > ~/.vim/bundle/vim-colorschemes/colors/molokai2.vim
+
 git config --global user.email "r.a.stratton88@gmail.com"
 git config --global user.name "Robert Stratton"
 git config --global color.ui "auto"
